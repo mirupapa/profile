4cc47edf5b19796730bcbeef19067db4508e8d87
mirupapa <mirutiropapa@icloud.com>
2022-01-22 15:07:53 +0900

    初期設定

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import styled from "styled-components"
+
+const FooterStyle = styled.div`
+  background: #515151;
+  height: 64px;
+`
+
+const Footer = () => {
+  return <FooterStyle />
+}
+
+export default Footer
