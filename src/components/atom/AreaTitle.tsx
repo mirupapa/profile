5d2e438cd72c4747914cda151216bb46d338ec01
mirupapa <mirutiropapa@icloud.com>
2022-01-23 19:09:44 +0900

    works実装

--- src/components/atom/AreaTitle.tsx
+++ src/components/atom/AreaTitle.tsx
@@ -1,7 +1,33 @@
-import React from "react"
+import React from 'react'
+import styled from 'styled-components'
 
-const AreaTitle: React.FC = () => {
-  return <></>
+const Wrapper = styled.div`
+  font-family: Roboto;
+  line-height: 28px;
+  text-align: center;
+`
+const MainTitle = styled.div`
+  font-weight: bold;
+  font-size: 40px;
+`
+const Subtitle = styled.div`
+  padding-top: 14px;
+  font-weight: 500;
+  font-size: 16px;
+`
+
+type Props = {
+  mainTitle: string
+  subTitle: string
+}
+
+const AreaTitle: React.FC<Props> = ({ mainTitle, subTitle }) => {
+  return (
+    <Wrapper>
+      <MainTitle>{mainTitle}</MainTitle>
+      <Subtitle>{subTitle}</Subtitle>
+    </Wrapper>
+  )
 }
 
 export default AreaTitle
--- src/components/organism/home/Works.tsx
+++ src/components/organism/home/Works.tsx
@@ -1,7 +1,63 @@
-import React from "react"
+import AreaTitle from 'components/atom/AreaTitle'
+import Work from 'components/molecule/Work'
+import React from 'react'
+import styled from 'styled-components'
+import Link from 'next/link'
+
+const Wrapper = styled.div`
+  height: 800px;
+  padding-top: 120px;
+`
+const Contents = styled.div`
+  display: flex;
+  width: 885px;
+  margin: 0 auto;
+  justify-content: space-between;
+`
+
+const loadingData = [
+  {
+    imgPath: '/mydiary.png',
+    imgAlt: 'myDiary',
+    mainTitle: 'MyDiary',
+    subTitle: 'Coding / React Native',
+  },
+  {
+    imgPath: '/dummy.png',
+    imgAlt: 'Dummy1',
+    mainTitle: 'Dummy1',
+    subTitle: 'Coding / React',
+  },
+  {
+    imgPath: '/dummy.png',
+    imgAlt: 'Dummy2',
+    mainTitle: 'Dummy2',
+    subTitle: 'Coding / JAVA',
+  },
+]
 
 const Works: React.FC = () => {
-  return <></>
+  return (
+    <Wrapper>
+      <AreaTitle mainTitle="WORKS" subTitle="制作実績など" />
+      <Contents>
+        {loadingData.map((data) => {
+          return (
+            <Link href={`/works/${data.mainTitle}`} key={data.mainTitle}>
+              <a>
+                <Work
+                  imgPath={data.imgPath}
+                  imgAlt={data.imgAlt}
+                  mainTitle={data.mainTitle}
+                  subTitle={data.subTitle}
+                />
+              </a>
+            </Link>
+          )
+        })}
+      </Contents>
+    </Wrapper>
+  )
 }
 
 export default Works
